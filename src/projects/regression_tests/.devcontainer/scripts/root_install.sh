394ce782021259b221b25aa6f2675d4f5e76bf6e
Anthony Brown <anthony.brown8@nhs.net>
2026-03-20 14:19:03 +0000

    no sudo

--- src/projects/regression_tests/.devcontainer/scripts/root_install.sh
+++ src/projects/regression_tests/.devcontainer/scripts/root_install.sh
@@ -6,12 +6,12 @@ set -e
 mkdir -p /etc/apt/keyrings
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/keyrings/google.asc >/dev/null
 if [ "$TARGETARCH" = "arm64" ] || [ "$TARGETARCH" = "aarch64" ]; then
-    sudo sh -c 'echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/google.asc] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sh -c 'echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/google.asc] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 else
-    sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google.asc] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google.asc] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 fi
-sudo apt-get update 
-sudo apt-get install -y google-chrome-stable
+apt-get update 
+apt-get install -y google-chrome-stable
 
 # clean up
 apt-get clean
