8f5e153078ec34b1605dfe8c349671ca1b3032c3
Anthony Brown <anthony.brown8@nhs.net>
2026-03-20 15:23:37 +0000

    try not using different arch for chrome

--- src/projects/regression_tests/.devcontainer/scripts/root_install.sh
+++ src/projects/regression_tests/.devcontainer/scripts/root_install.sh
@@ -5,11 +5,7 @@ set -euo pipefail
 # install chrome
 mkdir -p /etc/apt/keyrings
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/keyrings/google.asc >/dev/null
-if [ "$TARGETARCH" = "arm64" ] || [ "$TARGETARCH" = "aarch64" ]; then
-    sh -c 'echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/google.asc] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
-else
-    sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google.asc] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
-fi
+sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google.asc] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 apt-get update 
 apt-get install -y google-chrome-stable
 
