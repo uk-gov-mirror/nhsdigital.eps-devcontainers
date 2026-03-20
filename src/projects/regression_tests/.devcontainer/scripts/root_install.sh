3270814c340ef29d059aa3df9c8b03df2e32eff0
Anthony Brown <anthony.brown8@nhs.net>
2026-03-20 14:22:23 +0000

    better

--- src/projects/regression_tests/.devcontainer/scripts/root_install.sh
+++ src/projects/regression_tests/.devcontainer/scripts/root_install.sh
@@ -6,9 +6,9 @@ set -euo pipefail
 mkdir -p /etc/apt/keyrings
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/keyrings/google.asc >/dev/null
 if [ "$TARGETARCH" = "arm64" ] || [ "$TARGETARCH" = "aarch64" ]; then
-    sh -c 'echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/google.asc] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sh -c 'echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/google.asc] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 else
-    sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google.asc] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google.asc] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 fi
 apt-get update 
 apt-get install -y google-chrome-stable
